a8b3b64823562941d505a8c68dfd5bef5a9c0d8e
acerjt <acersjt@gmail.com>
2020-09-26 15:20:21 +0700

    add pug view engine

--- application/app.js
+++ application/app.js
@@ -14,6 +14,7 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+// app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
