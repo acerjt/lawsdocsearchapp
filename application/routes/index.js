ed32d68806292f0472130534550ec38fa906ef1a
DungNgoc <you@example.com>
2020-10-08 23:36:49 +0700

    Reponsive and fix content

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('home', { title: 'Express' });
+    res.render('home', { title: 'Trang chủ' });
 });
 
 
-router.get('/contact', function(req, res, next) {
-    res.render('contactUs', { title: 'Express' });
+router.get('/contactUs', function(req, res, next) {
+    res.render('contactUs', { title: 'Liên hệ' });
+});
+
+router.get('/termOfUse', function(req, res, next) {
+    res.render('termOfUse', { title: 'Điều khoản' });
+});
+
+router.get('/aboutUs', function(req, res, next) {
+    res.render('aboutUs', { title: 'Về chúng tôi' });
 });
 module.exports = router;
