7ef73efa85fa8875125a3ac6104285895e673308
acerjt <acersjt@gmail.com>
2020-11-07 13:17:12 +0700

    common use variable and function

---
+++ application/common.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+const filePathStoreLawsData = path.resolve(__dirname, "./data/lawsv2.json")
+const lawsIndex = 'laws1'
+
+
+module.exports = {filePathStoreLawsData, lawsIndex}
