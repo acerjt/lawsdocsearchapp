d10081a82daaa00f712706ff694aafd74614e932
acerjt <acersjt@gmail.com>
2020-11-07 13:20:51 +0700

    config elasticsearch connection

--- application/connection/elastic-connect.js
+++ application/connection/elastic-connect.js
@@ -1,5 +1,7 @@
 const { Client } = require('@elastic/elasticsearch')
-const client = new Client({ node: 'http://localhost:9200' , auth : { username: 'elastic', password: 'DT2403'}})
+// const client = new Client({ node: 'http://localhost:9200' , auth : { username: 'elastic', password: 'DT2403'}})
+const client = new Client({ node: 'http://localhost:9200' })
+
 
 
 module.exports = { client }
