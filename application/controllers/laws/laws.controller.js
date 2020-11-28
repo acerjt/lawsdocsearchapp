022c2c48eb884d5e8836e7ef5e6516309824ed7b
acerjt <acersjt@gmail.com>
2020-11-29 00:22:29 +0700

    implement method getLawsById to get data for render detailLaw page

--- application/controllers/laws/laws.controller.js
+++ application/controllers/laws/laws.controller.js
@@ -6,14 +6,25 @@ const {titles, pugFiles, lawsIndex, lawsSearchSize, maxResultWindow, lawsPagingI
 const getLawsInParticularPage = async (page) => {
     try {
         page = page && page > 1 ? Number(page) : 1
+        console.time('paging time')
         const {from, size, isOverTenThoudsandDocs, paginateDisplayConfiguration } =  await pagination(page)
+        console.timeEnd('paging time')
         if(!isOverTenThoudsandDocs) {
+            console.time('search time')
             let {body} = await client.search({
                 index : lawsIndex,
                 from: from,
                 size: size,
+                // filterPath: 'took,hits.hits',
+                // _source: [
+                //     "name",
+                //     "desc",
+                //     "issuedDate",
+                //     "effectiveDate",
+                //     "effectiveStatus",
+                // ],
                 body: {
-                    sort: {
+                    "sort": {
                         "issuedDate": {
                             "order": "desc"
                         },
@@ -23,9 +34,12 @@ const getLawsInParticularPage = async (page) => {
                     }
                 }
             })
+            console.timeEnd('search time')
             return Promise.resolve({ lawsDoc: body.hits.hits, page : page, paginateDisplayConfiguration : paginateDisplayConfiguration})
         }
         else {
+            console.time('search time')
+            console.time('search page id time')
             let {body} = await client.search({
                 index: lawsPagingIndex,
                 body: {
@@ -36,11 +50,23 @@ const getLawsInParticularPage = async (page) => {
                     }
                 }
             })
+            console.timeEnd('search page id time')
             if(body.hits.hits[0]) {
+                console.time('search page time')
                 let {lastLawsDocument, sortIssueDate} = body.hits.hits[0]._source
                 let result = await client.search({
                     index : lawsIndex,
                     size: lawsSearchSize,
+                    // _source: [
+                    //     "name",
+                    //     "desc",
+                    //     "docType",
+                    //     "docNum",
+                    //     "issuedDate",
+                    //     "effectiveDate",
+                    //     "numOfAnnouncement",
+                    //     "effectiveStatus",
+                    // ],
                     body: {
                         "query": {
                             "match_all": {}
@@ -57,7 +83,9 @@ const getLawsInParticularPage = async (page) => {
                             }
                         ]
                     }
-                })
+                })                
+                console.timeEnd('search page time')
+                console.timeEnd('search time')
                 return Promise.resolve({ lawsDoc: result.body.hits.hits, page : page, paginateDisplayConfiguration : paginateDisplayConfiguration})
             }
             return Promise.resolve({ lawsDoc: [], page : page, paginateDisplayConfiguration : paginateDisplayConfiguration})
@@ -98,19 +126,48 @@ const pagination = async (page) => {
 
     return { from : from, size : size, isOverTenThoudsandDocs : isOverTenThoudsandDocs, paginateDisplayConfiguration : paginateDisplayConfiguration}
 }
-
+const getLawById = async (lawDocId) => {
+    lawDocId = '/' + lawDocId
+    let {body} = await client.search({
+        index: lawsIndex,
+        body: {
+            "query" : {
+                "match" : {
+                    "href": lawDocId
+                }   
+            }
+        }
+    })
+    console.log(body)
+    return body.hits.hits[0]
+}
 module.exports.getLaws = async (req, res) => {
     try {
+        console.time('total time')
         let currentPage = req.query.page
         let lawsData = await getLawsInParticularPage(currentPage)
+        console.time('render time')
         res.render(pugFiles.home, {
             title: titles.home, 
             lawsDoc: lawsData.lawsDoc && lawsData.lawsDoc.length === 0 ? [] : lawsData.lawsDoc, 
             currentPage: lawsData.page,
             paginateDisplayConfiguration : lawsData.paginateDisplayConfiguration
         })
+        console.timeEnd('render time')
+        console.timeEnd('total time')
     } catch (error) {
         console.log(error)
         res.render(pugFiles.home, { title: titles.home, lawsDoc : []});
+    }
+}
+
+module.exports.getLawById = async (req,res) => {
+    try {
+        let lawsDocId = req.params.id
+        let lawDocument  = await getLawById(lawsDocId)
+        res.render(pugFiles.detailLaw, {lawDocument : lawDocument})
+    } catch (error) {
+        console.log(error)
+        res.render(pugFiles.error404)
     }
 }
